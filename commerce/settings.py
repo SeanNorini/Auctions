8d881b82aec8c7289127d46d5dd2d274724e97a0
Sean Norini <141059916+SeanNorini@users.noreply.github.com>
2023-10-16 23:20:46 -0400

    Integrated outside database and file storage

--- commerce/settings.py
+++ commerce/settings.py
@@ -164,5 +164,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 DEFAULT_FILE_STORAGE = 'commerce.storage_backends.MediaStorage'
